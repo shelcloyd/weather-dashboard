3f3aadf294dc362b62c72399822a42bbc87ea5d7
Shel Cloyd <shel.cloyd@gmail.com>
2023-01-23 12:22:30 -0800

    troubleshooting getting data to populate onto page

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# weather-dashboard
+# **Weather Dashboard**
+
+___
+
+#### **Table of Contents**
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [How to Use](#instructions)
+* [License](#license)
+* [Link to Application](#link)
+* [Contact](#contact)
+
+___
+
+<div id="description"></div>
+
+## **Description**
+This app allows you to check weather conditions in any given city.
+
+___
+
+<div id="installation"></div>
+
+## **Installation Instructions**
+Please visit https://shelcloyd.github.io/weather-dashboard/
+
+___
+
+<div id="usage"></div>
+
+## **Intended Usage**
+Weather Dashboard is intended to be used to search 5-day forecasted weather conditions in any given city.
+
+___
+
+<div id="contributions"></div>
+
+## **Contributions**
+Weather Dashboard was developed by Shel Cloyd.
+
+___
+
+<div id="instructions"></div>
+
+## **How to Use sample**
+* Please visit https://shelcloyd.github.io/weather-dashboard/
+* Type a city name into the search bar and click the search button
+
+___
+
+<div id="license"></div>
+
+## **License**
+* MIT License
+
+___
+
+<div id="link"></div>
+
+## **Link to Application**
+* https://shelcloyd.github.io/weather-dashboard/
+
+___
+
+<div id="contact"></div>
+
+## **Questions?**
+* GitHub: shelcloyd
+* Email:  shel.cloyd@gmail.com
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,36 +1,67 @@
-var inputValue = document.querySelector("#search-box");
-var search = document.querySelector("#search");
-var cityName = document.querySelector("#name");
-var temp = document.querySelector(".temp");
-var wind = document.querySelector(".wind");
-var humid = document.querySelector(".humid");
-var icon = document.querySelector(".icon");
+// var inputValue = document.querySelector("#search-box");
+// var search = document.querySelector("#search");
+// var cityName = document.querySelector("#name");
+// var temp = document.querySelector(".temp");
+// var wind = document.querySelector(".wind");
+// var humid = document.querySelector(".humid");
+// var icon = document.querySelector(".icon");
+// var forecastDate = document.querySelector(".date");
 
-function citySearch () {
-    
-}
-search.addEventListener("click", function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + "&appid=8fcfa15744ca25f903914bd30c91b962&units=imperial")
-        .then(resp => {
-            if (!resp.ok) alert("Invalid city name.");
-            return resp.json();
-        })
+// search.addEventListener("click", function () {
+//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + "&appid=8fcfa15744ca25f903914bd30c91b962&units=imperial")
+//         .then(resp => {
+//             if (!resp.ok) alert("Invalid city name.");
+//             return resp.json();
+//         })
+//         .then(data => {
+//             var cityNameValue = data["city"]["name"];
+//             var tempValue = data["list"][0]["main"]["temp"];
+//             var windValue = data["list"][0]["wind"]["speed"];
+//             var humidValue = data["list"][0]["main"]["humidity"];
+//             var iconValue = data["list"][0]["weather"][0]["icon"];
+
+
+//             cityName.innerHTML = cityNameValue;
+//             temp.innerHTML = "Temperature: " + tempValue + "°F";
+//             wind.innerHTML = "Wind Speed: " + windValue + "mph";
+//             humid.innerHTML = "Humidity: " + humidValue + "%";
+//             icon.innerHTML = '<img src="http://openweathermap.org/img/wn/' + iconValue + '@2x.png">';
+
+//             console.log(data);
+//         })
+//         .catch(console.err);
+// });
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('#search');
+const cityName = document.querySelector('#name');
+const apiKey = '8fcfa15744ca25f903914bd30c91b962';
+
+btn.addEventListener('click', function () {
+    cityName.innerHTML = input.value;
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input.value + '&appid=' + apiKey + '&units=imperial')
+        .then(response => response.json())
+        .then(data => console.log(data))
         .then(data => {
-            var cityNameValue = data["city"]["name"];
-            var tempValue = data["list"][0]["main"]["temp"];
-            var windValue = data["list"][0]["wind"]["speed"];
-            var humidValue = data["list"][0]["main"]["humidity"];
-            var iconValue = data["list"][0]["weather"][0]["icon"];
+            const day = document.querySelector('.day');
+            const icon = document.querySelector('.icon');
+            const temp = document.querySelector('.temp');
+            const wind = document.querySelector('.wind');
+            const humidity = document.querySelector('.humidity');
+            for (i = 0; i < 5; i++) {
+                let dayValue = data['list'][i]['dt'];
+                let tempValue = data["list"][i]["main"]["temp"];
+                let windValue = data["list"][i]["wind"]["speed"];
+                let humidityValue = data["list"][i]["main"]["humidity"];
+                let iconValue = data["list"][i]["weather"]["icon"];
 
-
-            cityName.innerHTML = cityNameValue;
-            temp.innerHTML = "Temperature: " + tempValue + "°F";
-            wind.innerHTML = "Wind Speed: " + windValue + "mph";
-            humid.innerHTML = "Humidity: " + humidValue + "%";
-            icon.innerHTML = '<img src="http://openweathermap.org/img/wn/' + iconValue + '@2x.png">';
-
-            console.log(data);
+                day.innerHTML = dayValue;
+                temp.innerHTML = "Temperature: " + tempValue + "°F";
+                wind.innerHTML = "Wind Speed: " + windValue + "mph";
+                humidity.innerHTML = "Humidity: " + humidityValue + "%";
+                icon.innerHTML = '<img src="http://openweathermap.org/img/wn/' + iconValue + '@2x.png">';
+            }
         })
-        .catch(console.err);
-})
-
+        .catch(err => alert('No data found.'))
+});
